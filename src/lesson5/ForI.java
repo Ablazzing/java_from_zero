b8ae295a65a994b1e4a293c2fb52ab28f6f0e230
Ablazzing <y2286122@yandex.ru>
2022-11-30 19:28:59 +0300

    For i

---
+++ src/lesson5/ForI.java
@@ -0,0 +1,23 @@
+package lesson5;
+
+public class ForI {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4};
+        //Первые три числа
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Значение - i:" + i);
+            System.out.println(numbers[i]);
+        }
+
+        //Вывод в перевернутом порядке
+        for (int i = 3; i >= 0 ; i--) {
+            System.out.println(numbers[i]);
+        }
+
+        //Все элементы массива, кроме последних двух
+        for (int i = 0; i < numbers.length - 2; i++) {
+            System.out.println(numbers[i]);
+        }
+
+    }
+}
