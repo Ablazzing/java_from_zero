95b23d260379beb3f2c2021b1e74de35b80fbc19
Ablazzing <y2286122@yandex.ru>
2022-11-30 22:46:14 +0300

    String класс

---
+++ src/lesson6/MyString.java
@@ -0,0 +1,14 @@
+package lesson6;
+
+public class MyString {
+    public static void main(String[] args) {
+        char symbol = 'a';
+        String text = new String("\they");
+        String text2 = new String("hey");
+        System.out.println(text.equals(text2));
+
+        System.out.println(text);
+        System.out.println(text2);
+
+    }
+}
