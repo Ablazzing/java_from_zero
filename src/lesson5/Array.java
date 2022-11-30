16713f7bc8949c3b5d56ce3f0f1a172eb16e3d72
Ablazzing <y2286122@yandex.ru>
2022-11-30 18:43:12 +0300

    Разница между примитивами и ссылочным типом данных

--- src/lesson5/Array.java
+++ src/lesson5/Array.java
@@ -8,14 +8,21 @@ import java.util.Arrays;
 public class Array {
     public static void main(String[] args) {
         int x = 5;
-        int y = 10;
-//        int[] array = new int[2];
-//        array[0] = x;
-//        array[1] = y;
-//        int [] array = new int[]{x, y};
-        int [] array = {x, y};
-        System.out.println(Arrays.toString(array));
+        int y = x;
+        x = 10;
+        System.out.println(x);
+        System.out.println(y);
 
-        System.out.println(array[1]);
+        int[] array = {x, y};
+        int[] array2 = new int[2];
+        array2[0] = array[0];
+        array2[1] = array[1];
+        array2[0] = 13;
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array2));
+        //Практическое задание
+        //Ответить на вопрос "Столица Катара?" и записать в массив (какого то типа)
+        char[] answer = {'Д', 'о', 'х', 'a'};
+
     }
 }
