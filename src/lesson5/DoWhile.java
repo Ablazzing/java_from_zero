20c6e00dcd3a063551db4258b908583df153925e
Ablazzing <y2286122@yandex.ru>
2022-11-30 21:13:53 +0300

    do while

---
+++ src/lesson5/DoWhile.java
@@ -0,0 +1,16 @@
+package lesson5;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int count = 0;
+        do {
+            System.out.println(count);
+            count++;
+        } while (count < 0);
+
+        System.out.println("Проверка неработоспособности while");
+        while (count < 0) {
+            System.out.println(count);
+        }
+    }
+}
