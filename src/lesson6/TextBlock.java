1787aef8cf9e08f48059b3104dc5deac31e705d9
Ablazzing <y2286122@yandex.ru>
2022-12-03 12:24:22 +0300

    Text block

---
+++ src/lesson6/TextBlock.java
@@ -0,0 +1,15 @@
+package lesson6;
+
+public class TextBlock {
+    public static void main(String[] args) {
+        //String text = "Привет, как тебя зовут?\nМеня зовут Юрий. А тебя?\nМеня зовут Петр!";
+        String name1 = "Гена";
+        String name2 = "Вася";
+        String text = """
+                Привет, как тебя зовут?
+                Меня зовут %s. А тебя?
+                Меня зовут %s!
+                """.formatted(name1, name2);
+        System.out.println(text);
+    }
+}
