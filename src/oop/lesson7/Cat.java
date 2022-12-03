dd1de88829f8a88e7621011c82a89bf4b8372ef0
Ablazzing <y2286122@yandex.ru>
2022-12-03 21:44:53 +0300

    Атрибуты

--- src/oop/lesson7/Cat.java
+++ src/oop/lesson7/Cat.java
@@ -1,10 +1,12 @@
 package oop.lesson7;
 
 public class Cat {
+    //Атрибут (свойство, поле)
     String color;
     double weight;
     String ownerName;
 
+    //Конструктор
     public Cat(String colorName, double weightCat, String owner) {
       this.color = colorName;
       this.weight = weightCat;
--- src/oop/lesson7/Runner.java
+++ src/oop/lesson7/Runner.java
@@ -3,6 +3,10 @@ package oop.lesson7;
 public class Runner {
     public static void main(String[] args) {
         Cat cat = new Cat("Black", 10, "Petr");
-        System.out.println(cat);
+        System.out.println(cat.color);
+        System.out.println(cat.ownerName);
+        System.out.println(cat.weight);
+        cat.color = "Red";
+        System.out.println(cat.color);
     }
 }
