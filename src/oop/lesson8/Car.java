8e2b9a7ced5b5583f2e8673fd1dbabc4f607e27d
Ablazzing <y2286122@yandex.ru>
2022-12-15 17:00:51 +0300

    Статическая инициализация

--- src/oop/lesson8/Car.java
+++ src/oop/lesson8/Car.java
@@ -1,10 +1,22 @@
 package oop.lesson8;
 
+import java.util.Arrays;
+
 public class Car {
     private String brand;
     //Статический атрибут
     public static int countCars = 0;
     public final static String BMW_INFO = "Bmw - немецкая компания, основанная 1928";
+    public final static String AUDI_INFO = "Audi - немецкая компания, основанная 1933";
+    public final static String KIA_INFO = "Kia - корейская компания, основанная 1978";
+    public final static String[] BRAND_DESCRIPTIONS = new String[3];
+
+    static {
+        BRAND_DESCRIPTIONS[0] = BMW_INFO;
+        BRAND_DESCRIPTIONS[1] = AUDI_INFO;
+        BRAND_DESCRIPTIONS[2] = KIA_INFO;
+        System.out.println(Arrays.toString(BRAND_DESCRIPTIONS));
+    }
 
     public Car(String brand) {
         this.brand = brand;
--- src/oop/lesson8/Runner.java
+++ src/oop/lesson8/Runner.java
@@ -1,12 +1,13 @@
 package oop.lesson8;
 
+import java.util.Arrays;
+
 public class Runner {
     public static void main(String[] args) {
         Car audi = new Car("audi");
         Car bmw = new Car("bmw");
-        System.out.println("Количество машин:" + Car.countCars);
 
-        Car.printBrands();
-        System.out.println(Car.BMW_INFO);
+        Car.BRAND_DESCRIPTIONS[0] = "Любое значение";
+        System.out.println(Arrays.toString(Car.BRAND_DESCRIPTIONS));
     }
 }
